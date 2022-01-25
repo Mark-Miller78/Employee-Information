c6a1cf4fce3d4dc583e9291b2dc2f93dd303611f
Mark Miller <markmiller7858@gmail.com>
2022-01-24 22:04:24 -0500

    welcome art done

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const inquire = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const figlet = require('figlet');
+
+const log= console.log;
 
 require('dotenv').config();
 
 //connect to database
-
 const db = mysql.createConnection(
     {
-        host: 'localhose',
+        host: 'localhost',
         user:'root',
-        password:process.env.DB_PW,
+        password: process.env.DB_PW,
         database: 'roster'
     },
-    console.log('Connected to the roster database')
 );
+
+db.connect(err=>{
+    if (err) throw err;
+    log('Connected to the roster database!');
+    welcomeSign();
+});
+
+welcomeSign =()=>{
+    log(`================================================================================================================`);
+    log(``);
+    log(``);
+    log(figlet.textSync('Employee Information'));
+    log(``);
+    log(``);
+    log(`================================================================================================================`);
+}
