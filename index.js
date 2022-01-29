9dcb3b0e7cb7a1423e5996a9b6e3da504aec2f51
Mark Miller <markmiller7858@gmail.com>
2022-01-29 17:30:53 -0500

    edits, view by, and deletes added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquire = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const figlet = require('figlet');
+const { prompt } = require('inquirer');
 
 const log= console.log;
 
@@ -240,6 +241,7 @@ addRoles=()=>{
                     type: 'list',
                     name:'deptList',
                     message: 'Select the Department the role belongs too.',
+                    loop: false,
                     choices: dept
                 }
             ])
@@ -315,6 +317,7 @@ addEmployees=()=>{
                     type:'list',
                     name:'roleinput',
                     message:'Select the role of the employee.',
+                    loop: false,
                     choices: roles
                 }
             ])
@@ -337,6 +340,7 @@ addEmployees=()=>{
                             type:'list',
                             name:'manager',
                             message: 'Select the manager the employee is assigned to.',
+                            loop: false,
                             choices:manager,
                             when:({confirmManager})=>{
                                 if(confirmManager === true){
@@ -364,3 +368,223 @@ addEmployees=()=>{
         });
     });
 };
+
+editEmployeeRole=()=>{
+    const employeeSql = `SELECT id, CONCAT (first_name," ",last_name) AS name FROM employees`;
+
+    db.query(employeeSql, (err, rows)=>{
+        if (err) throw err;
+        const employees = rows.map(({id, name})=>({value: id, name: name}));
+
+        inquire.prompt([
+            {
+                type: 'list',
+                name:'choice',
+                message:'Pick the employee you would like to edit.',
+                loop: false,
+                choices: employees
+            }
+        ])
+        .then(answer=>{
+            let params = [answer.choice];
+
+            const roleSql = ` SELECT id, title FROM roles`;
+            db.query(roleSql, (err, rows)=>{
+                if (err) throw err;
+                const roles = rows.map(({id, title})=>({value: id, name:title}));
+
+                inquire.prompt([
+                    {
+                        type:'list',
+                        name:'newRole',
+                        message:'Select the new role for this employee.',
+                        loop: false,
+                        choices: roles,
+                    }
+                ])
+                .then(answer=>{
+                    params.unshift(answer.newRole);
+
+                    newRoleSql = `UPDATE employees SET role_id = ? WHERE id = ?`;
+                    db.query(newRoleSql, params, (err, rows)=>{
+                        if(err) throw err;
+                        log('New roles succesfully assigned!');
+
+                        getEmployees();
+                    })
+                })
+            })
+        })
+    })
+};
+
+editEmployeeManager=()=>{
+    const employeeSql = `SELECT id, CONCAT (first_name," ",last_name) AS name FROM employees`;
+
+    db.query(employeeSql, (err, rows)=>{
+        if (err) throw err;
+        const employees = rows.map(({id, name})=>({value: id, name: name}));
+
+        inquire.prompt([
+            {
+                type: 'list',
+                name:'choice',
+                message:'Pick the employee you would like to edit.',
+                loop: false,
+                choices: employees
+            },
+            {
+                type: 'list',
+                name:'manager',
+                message:'Pick the new manager you would like to assign to this employee.',
+                loop: false,
+                choices: employees
+            }
+        ])
+        .then(answers=>{
+            const params = [answers.manager, answers.choice];
+            const sql = `UPDATE employees SET manager_id = ? WHERE id = ?`;
+
+            db.query(sql, params, (err, rows)=>{
+                if(err) throw err;
+                log("Employee's manager successfully updated!");
+
+                getEmployees();
+            })
+        })
+    })
+};
+
+employeesByManager=()=>{
+    const managerSql = `SELECT id, CONCAT (first_name," ",last_name) AS name FROM employees`;
+
+    db.query(managerSql, (err, rows)=>{
+        if (err) throw err;
+        const managers = rows.map(({id, name})=>({value: id, name: name}));
+
+        inquire.prompt([
+            {
+                type:'list',
+                name:'managers',
+                message:"Select which manager's employees you would like to view.",
+                loop: false,
+                choices: managers
+            }
+        ])
+        .then(answer=>{
+            const sql = `SELECT employees.id AS Id, CONCAT (employees.first_name," ", employees.last_name) AS Name, roles.title AS Position, departments.name AS Department, roles.salary AS Salary, CONCAT (manager.first_name, " ", manager.last_name) AS Manager
+                        FROM employees 
+                        LEFT JOIN roles ON employees.role_id = roles.id
+                        LEFT JOIN departments ON roles.department_id=departments.id
+                        LEFT JOIN employees manager ON employees.manager_id=manager.id
+                        WHERE employees.manager_id = ?`
+            const params = [answer.managers];
+
+            db.query(sql, params,(err, rows)=>{
+                if(err) throw err;
+                log('');
+                log('Viewing employees assigned to selected manager');
+                log('');
+                console.table(rows);
+                promptUser();
+            })
+        })
+    })
+};
+
+employeesByDepartment=()=>{
+    const deptsql=`SELECT departments.id AS ID, departments.name AS Department FROM departments`;
+
+    db.query(deptsql, (err, rows)=>{
+        if (err) throw err;
+        const departments = rows.map(({ID, Department})=>({value: ID, name:Department}));
+
+        inquire.prompt([
+            {
+                type:'list',
+                name:'departments',
+                message:"Select which department's employees you would like to view.",
+                loop:false,
+                choices: departments,
+            }
+        ])
+        .then(answer=>{
+            const sql = `SELECT employees.id AS Id, CONCAT (employees.first_name," ", employees.last_name) AS Name, roles.title AS Position
+                        FROM employees 
+                        LEFT JOIN roles ON employees.role_id = roles.id
+                        LEFT JOIN departments ON roles.department_id=departments.id
+                        WHERE roles.department_id = ?`
+            const params = [answer.departments]
+            db.query(sql, params, (err, rows)=>{
+                if(err) throw err;
+                log('');
+                log('Viewing employees by selected department.');
+                log('');
+                console.table(rows);
+                promptUser();
+            })
+        })
+    });
+};
+
+deleteDepartment=()=>{
+    const deptSql = `SELECT departments.id AS ID, departments.name AS Department FROM departments`;
+
+    db.query(deptSql, (err, rows)=>{
+        if (err) throw err;
+        const departments = rows.map(({ID, Department})=>({value: ID, name:Department}));
+
+        inquire.prompt([
+            {
+                type:'list',
+                name:'deleteDept',
+                message:'Select the Department you would like to delete.',
+                loop: false,
+                choices: departments
+            }
+        ])
+        .then(answer=>{
+            const sql =`DELETE FROM departments WHERE id = ?`;
+            const params = [answer.deleteDept];
+
+            db.query(sql, params, (err, rows)=>{
+                if (err) throw err;
+                log('');
+                log('Department Successfully Deleted!')
+                log('');
+                getDepartments();
+            })
+        })
+    })
+};
+
+deleteRole=()=>{
+    const roleSql =`SELECT id, title FROM roles`;
+
+    db.query(roleSql,(err,rows)=>{
+        if(err) throw err;
+        const roles = rows.map(({id, title})=>({value:id, name:title}));
+
+        inquire.prompt([
+            {
+                type:'list',
+                name:'roleList',
+                message:'Select which role should be deleted.',
+                loop: false,
+                choices: roles
+            }
+        ])
+        .then(answer=>{
+            const sql = `DELETE FROM roles WHERE id = ?`
+            const params = [answer.roleList];
+
+            db.query(sql, params, (err, rows)=>{
+                if (err) throw err;
+                log('');
+                log('Role succesfully deleted');
+                log('');
+                getRoles();
+            })
+        })
+    })
+}
